a0b0b338d5f7fff55b96b80eae3193ed053a479d
vpfl95 <k_dh610@naver.com>
2023-04-10 20:04:10 +0900

    JdbcTemplate - 이름 지정 파라미터 1

---
+++ src/main/java/hello/itemservice/repository/jdbctemplate/JdbcTemplateItemRepositoryV2.java
@@ -0,0 +1,120 @@
+package hello.itemservice.repository.jdbctemplate;
+
+import hello.itemservice.domain.Item;
+import hello.itemservice.repository.ItemRepository;
+import hello.itemservice.repository.ItemSearchCond;
+import hello.itemservice.repository.ItemUpdateDto;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.util.StringUtils;
+
+import javax.sql.DataSource;
+import java.sql.PreparedStatement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+/**
+ * NamedParameterJdbcTemplate
+ * SqlParameterSource
+ * - BeanPropertySqlParameterSource
+ * - MapSqlParameterSource
+ * Map
+ *
+ * BeanPropertyRowMapper
+ *
+ */
+@Slf4j
+public class JdbcTemplateItemRepositoryV2 implements ItemRepository {
+
+    // 파라미터를 순서가 아니라 이름 기반으로함
+    private final NamedParameterJdbcTemplate template;
+
+    public JdbcTemplateItemRepositoryV2(DataSource dataSource) {
+        this.template = new NamedParameterJdbcTemplate(dataSource);
+    }
+
+    @Override
+    public Item save(Item item) {
+        String sql = "insert into item(item_name, price, quantity) values(:itemName, :price, :quantity) ";
+
+        // 파라미터 만드는 방법1. item을 가지고 파라미터를 만듬 itemName, price, quantity
+        SqlParameterSource param = new BeanPropertySqlParameterSource(item);
+
+        //db에서 생성해준 id값 가져오기
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        template.update(sql,param, keyHolder);
+
+        long key = keyHolder.getKey().longValue();
+        item.setId(key);
+        return item;
+    }
+
+    @Override
+    public void update(Long itemId, ItemUpdateDto updateParam) {
+        String sql = "update item set item_name:itemName, price=:price, quantity=:quantity where id=:id";
+
+        //방법2
+        SqlParameterSource param = new MapSqlParameterSource()
+                .addValue("itemName", updateParam.getItemName())
+                .addValue("price", updateParam.getPrice())
+                .addValue("quantity", updateParam.getQuantity())
+                .addValue("id", itemId);
+
+        template.update(sql,param);
+    }
+
+    @Override
+    public Optional<Item> findById(Long id) {
+        String sql = "select id, item_name, price, quantity from item where id=:id";
+        try {
+            //방법3
+            Map<String, Long> param = Map.of("id", id);
+            Item item = template.queryForObject(sql, param, itemRowMapper());
+            return Optional.of(item);
+        }catch (EmptyResultDataAccessException e){
+            return Optional.empty();
+        }
+    }
+
+    private RowMapper<Item> itemRowMapper() {
+        return BeanPropertyRowMapper.newInstance(Item.class);
+    }
+
+    @Override
+    public List<Item> findAll(ItemSearchCond cond) {
+        String itemName = cond.getItemName();
+        Integer maxPrice = cond.getMaxPrice();
+
+        SqlParameterSource param = new BeanPropertySqlParameterSource(cond);
+
+        String sql = "select id, item_name, price, quantity from item";
+        //동적 쿼리
+        if (StringUtils.hasText(itemName) || maxPrice != null) {
+            sql += " where";
+        }
+        boolean andFlag = false;
+        if (StringUtils.hasText(itemName)) {
+            sql += " item_name like concat('%',:itemName,'%')";
+            andFlag = true;
+        }
+        if (maxPrice != null) {
+            if (andFlag) {
+                sql += " and";
+            }
+            sql += " price <= :maxPrice";
+        }
+        log.info("sql={}", sql);
+        return template.query(sql, param, itemRowMapper());
+    }
+}
